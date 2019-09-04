2c53a3120c56b0d914af08c03597e167a0d740cb
Altaf M Quadri <altafmquadri@gmail.com>
2019-09-04 23:41:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,11 +12,11 @@ def reduce(array, sp = 0)
   sum = sp
   i = 0
   while i < array.length
-    sum = yield(sum, array[i])
+    sum = yield(array[i], sum)
     i+=1
   end
-  if sum != nil
-    return true
+  if sum == nil
+    return false
   end
   sum
 end
